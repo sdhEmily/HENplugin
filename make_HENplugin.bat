8a9f7dcaf2759fffc1e4ed5e136b4295b402cd09
Emily <143989797+sdhEmily@users.noreply.github.com>
2024-12-25 02:24:58 +0000

    Add PS3HEN.BIN packing

--- make_HENplugin.bat
+++ make_HENplugin.bat
@@ -1,11 +1,18 @@
 @echo off
 cls
-set PS3SDK=/c/PSDK3v2
+set PS3SDK=/c/PSDK3v2   q
 set WIN_PS3SDK=C:/PSDK3v2
 set PS3DEV=%PS3SDK%/ps3dev2
 set PATH=%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%PS3DEV%/ppu/bin;
 set CYGWIN=C:\PSDK3v2\MinGW\msys\1.0\bin
 
+
+
+:: thanks to https://github.com/kapodamy/HENtranslationTools
+HEN_UNGEN PS3HEN.BIN 
+if %ERRORLEVEL% EQU -1 echo Please place PS3HEN.BIN into this directory. && pause && exit
 rm *.prx *.sprx
 %CYGWIN%\bash -i -c 'make release'
+HEN_GEN PS3HEN.BIN stage2.bin stage0.bin HENplugin.sprx
+if %ERRORLEVEL% EQU 0 echo PS3HEN.BIN was updated with your HENplugin build.
 pause
